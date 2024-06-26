5542484dc2b782e1b372ec509b3994807c0a17d3
tanishakhabe <tkhabe@andrew.cmu.edu>
2024-06-26 14:17:54 -0400

    added navigationlinks to the quiz and fundraisers on the home page and started creating one of the questions on SecondView

--- EcoImpact/ContentView.swift
+++ EcoImpact/ContentView.swift
@@ -9,9 +9,15 @@ struct ContentView: View {
             Image(systemName: "leaf")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
+            Text("EcoImpact")
             NavigationLink(destination: SecondView()) {
-                    Text("EcoImpact")
-                }
+                Text("Take the Sustainability Quiz!")
+                    .frame(width: 200.0, height: /*@START_MENU_TOKEN@*/50.0/*@END_MENU_TOKEN@*/)
+            }
+            NavigationLink(destination: ThirdView()) {
+                Text("Donate ♥︎")
+                    .frame(width: /*@START_MENU_TOKEN@*/200.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/50.0/*@END_MENU_TOKEN@*/)
+            }
             
                 .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)        }
         .padding()
--- EcoImpact/SecondView.swift
+++ EcoImpact/SecondView.swift
@@ -6,9 +6,35 @@
 //
 import SwiftUI
 struct SecondView: View {
+    
+    @State private var gallons = "_"
     var body: some View {
-        NavigationLink(destination: ThirdView()) {
-            Text("Quiz")
+        NavigationStack {
+            VStack {
+                Text("Environmental Sustainability Quiz")
+                    .font(.largeTitle)
+                Text("How much time do you spend in the shower each day?")
+                      HStack {
+                        Button("5 min") {
+                          gallons = "10"
+                        }
+                        Button("6-15 min") {
+                          gallons = "20"
+                        }
+                        Button("16-30 min") {
+                          gallons = "45"
+                        }
+                        Button("31-45 min") {
+                          gallons = "75"
+                        }
+                        Button("45+ min") {
+                          gallons = "90+"
+                        }
+                      }
+                Text("You use \(gallons) of water to shower each day.")
+
+                
+            }
         }
     }
     
