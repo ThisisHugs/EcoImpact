23045d14a6e9b383a5fdbcbc9400c7e6927414a7
tanishakhabe <tkhabe@andrew.cmu.edu>
2024-06-27 11:39:58 -0400

    finished first question and added navlink to third view

--- EcoImpact/SecondView.swift
+++ EcoImpact/SecondView.swift
@@ -5,42 +5,65 @@
 //  Created by Abigail Boshart on 6/26/24.
 //
 import SwiftUI
+
+
 struct SecondView: View {
     
     @State private var gallons = "_"
+    @State private var suggestion = ""
+    @State private var showSuggestion = false
+    
     var body: some View {
         NavigationStack {
             VStack {
-                Text("Environmental Sustainability Quiz")
-                    .font(.largeTitle)
                 Text("How much time do you spend in the shower each day?")
-                      HStack {
-                        Button("5 min") {
-                          gallons = "10"
-                        }
-                        Button("6-15 min") {
-                          gallons = "20"
-                        }
-                        Button("16-30 min") {
-                          gallons = "45"
-                        }
-                        Button("31-45 min") {
-                          gallons = "75"
-                        }
-                        Button("45+ min") {
-                          gallons = "90+"
-                        }
-                      }
+                HStack {
+                    Button("5 min") {
+                        gallons = "10"
+                        suggestion = "You're already saving a lot of water!"
+                        showSuggestion = false
+                    }
+                    Button("6-15 min") {
+                        gallons = "20"
+                        suggestion = "You're doing a pretty good job having short showers!"
+                        showSuggestion = false
+                    }
+                    Button("16-30 min") {
+                        gallons = "45"
+                        suggestion = "This is the amount of time most people spend in the shower. It is on the less eco-friendly side. Try taking shorter showers to save more water! "
+                        showSuggestion = false
+                    }
+                    Button("31-45 min") {
+                        gallons = "75"
+                        suggestion = "Two showers could water a medium-sized tree! Try taking shorter showers to save more water!"
+                        showSuggestion = false
+                    }
+                    Button("45+ min") {
+                        gallons = "90+"
+                        suggestion = "75% of water used in houses is used! the bathroom. Try taking shorter showers to save more water."
+                        showSuggestion = false
+                    }
+                }
                 Text("You use \(gallons) of water to shower each day.")
-
+                Button("Get my suggestion!") {
+                    //Text("\(suggestion)")
+                    showSuggestion = true
+                    
+                }
+                if showSuggestion {
+                    Text("\(suggestion)")
+                }
+                NavigationLink(destination: ThirdView()){
+                    Text("Next Question")
+                }
                 
             }
+                
         }
-    }
-    
-    #Preview {
-        ThirdView()
+        .padding()
     }
 }
 
-
+#Preview {
+    SecondView()
+}
