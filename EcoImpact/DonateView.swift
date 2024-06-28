893b496731c4af82fb0b745435381b554c175269
Abigail <abigailsspaceship@gmail.com>
2024-06-28 10:24:26 -0400

    Donation View

--- EcoImpact/DonateView.swift
+++ EcoImpact/DonateView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct DonateView: View {
+@State private var totalDonations = 0
     var body: some View {
         NavigationStack {
             ZStack {
@@ -19,11 +20,43 @@ struct DonateView: View {
                     Text("Fundraisers & Organizations")
                         .foregroundColor(Color(hue: 0.742, saturation: 0.193, brightness: 0.579))
                         .font(.title)
-                        .fontWeight(.bold)
-                    HStack {
-                    }
-                }
-            }
+                        .fontWeight(.bold)                            
+                   
+                            
+
+            VStack {
+                                    Text("Fundraisers")
+                                        .font(.title)
+                                        .padding()
+                                    
+                                    Text("Click On A Star To Donate!")
+                                        .padding()
+                                    
+                                    Text("All the donations go to the climate change cause and organizations and fundraisers down below!")
+                                        .padding()
+                                    
+                                    Text("Do your part to help the environment!")
+                                        .padding()
+                                    
+                                    Button(action: {
+                    
+                                        totalDonations += 5
+                                    
+                                    }) {
+                                        Label("$5", systemImage: "star.fill")
+                                            .labelStyle(.iconOnly)
+                                    }
+                                    .padding()  
+                Text("Raised: \(totalDonations) so far.")
+                                }
+                            }
+                        }
+
+            
+                        
+
+
+            
             
         }
         
