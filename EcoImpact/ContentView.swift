0c56aa007c4a3cf8509793434526f7cb606ff1c3
Emily Luu <tangerine2078@gmail.com>
2024-06-27 20:14:11 -0400

    Changed background color, other designs in main page

--- EcoImpact/ContentView.swift
+++ EcoImpact/ContentView.swift
@@ -3,34 +3,65 @@
 import SwiftUI
 struct ContentView: View {
     var body: some View {
-        NavigationStack {
-        VStack {
-           
-            Image(systemName: "leaf")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("EcoImpact")
-            
-            Text("Hi! This is EcoImpact, an app that helps give sustainable lifestyle suggestions. Together, little by little, we each can help make a difference and contribute to a greener world")
-                .multilineTextAlignment(.center)
-                .padding()
-                .foregroundColor(Color.green)
-            
-            NavigationLink(destination: SecondView()) {
-                Text("Take the Sustainability Quiz!")
-                    .frame(width: 200.0, height: /*@START_MENU_TOKEN@*/50.0/*@END_MENU_TOKEN@*/)
-            }
-            NavigationLink(destination: ThirdView()) {
-                Text("Donate ♥︎")
-                    .frame(width: /*@START_MENU_TOKEN@*/200.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/50.0/*@END_MENU_TOKEN@*/)
-            }
-            
-                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)        }
-        .padding()
-            }
         
-    }
+        
+        NavigationStack {
+            VStack {
+                
+                Image(systemName: "leaf")
+                    .renderingMode(.template)
+                    .foregroundColor(Color(hue: 0.44, saturation: 0.266, brightness: 0.587))
+                    .fontWeight(.bold)
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                
+                Text("EcoImpact")
+                    .foregroundColor(Color(hue: 0.742, saturation: 0.193, brightness: 0.579))
+                    .font(.title)
+                    .fontWeight(.bold)
+                
+                Text("Hi! This is EcoImpact, an app that helps give sustainable lifestyle suggestions. Together, little by little, we each can help make a difference and contribute to a greener world.")
+                    .font(.callout)
+                    .multilineTextAlignment(.center)
+                    .padding()
+                    .foregroundColor(Color(hue: 0.434, saturation: 0.278, brightness: 0.553))
+                
+                NavigationLink(destination: SecondView()) {
+                    Text("Take the Sustainability Quiz!")
+                        .font(.callout)
+                        .foregroundColor(Color(hue: 1.0, saturation: 0.117, brightness: 0.303))
+                        .frame(width: 200.0, height: /*@START_MENU_TOKEN@*/50.0/*@END_MENU_TOKEN@*/)
+                }
+                .foregroundColor(.black)
+                .background {
+                    Color("Color2")
+                        .cornerRadius(15)
+                }
+                .padding()
+                
+                NavigationLink(destination: ThirdView()) {
+                    Text("Donate ♥︎")
+                        .font(.callout)
+                        .foregroundColor(Color(hue: 1.0, saturation: 0.117, brightness: 0.303))
+                    .frame(width: 200.0, height: /*@START_MENU_TOKEN@*/50.0/*@END_MENU_TOKEN@*/)
+                        .frame(width: /*@START_MENU_TOKEN@*/200.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/50.0/*@END_MENU_TOKEN@*/)
+                }
+                .background {
+                    Color("Color2")
+                        .cornerRadius(15)
+                }
+                .padding()
+                }
+        
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background {
+                    Color("Custom Color")
+                        .ignoresSafeArea()
+                }
+            }
+        }
 }
+    
 #Preview {
     ContentView()
 }
